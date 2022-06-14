956c911353a304a9799be79e4d96265fafefe5a7
Mikkbrya <89813302+Mikkbrya@users.noreply.github.com>
2022-06-13 20:01:59 -0400

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,67 @@
+function setup()
+{
+    createCanvas(1000, 1000);
+}
+                function draw(){
+                    background(0,0,0);
+                    let cir = color(253,132,53);
+                    fill(cir);
+                    circle(x,y size);
+this.x=500;
+this.y=500;
+this.size=1000;
+                }
+
+
+(function()) {
+    let canvas,ctx,gravity,ball,friction
+    function init(){
+    canvas=document.getElementById('gameCanvas')
+    ctx=canvas.getContext('2d')
+    canvas.width = 800;
+    canvas.height = 800;
+                        gravity=0.25;
+                        friction=0.98;
+    window.requestAnimationFrame(update){
+    function draw(){
+    ctx.clearRect(0,0,canvas,width,canvas.height) {
+    function update (){
+    window.requestAnimationFrame(update)
+                            draw() {
+     document.addEventListener('DOMCContentLoaded',init)                           
+                            }
+          }
+        }
+      }
+    }
+  }
+}
+
+using AdminSystem;
+namespace Adminpp
+{
+class WriteClientRecords
+}
+static void main()
+int id;
+string clientName;
+double balance;
+AdminApp.Write("Enter client id :");
+id = int.Parse(Console.ReadLine());
+Console.Write("Enter client name: ");
+clientName = Console.ReadLine();
+Console.Write =("Enter client balance :");
+balance = double.Parse(Console.ReadLine());
+Client p = new Client(id, clientName, balance);
+p.writeToFile();
+Console.WriteLine("Record has been added to file");
+Console.ReadKey();
+}
+    }
+      }
+
+
+
+
+
+
